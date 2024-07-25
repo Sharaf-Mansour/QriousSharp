99495a3ff0877f2fc6a59cf050e801f92939c65c
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-07-25 08:01:43 +0300

    Create BlazorQR.js

---
+++ wwwroot/BlazorQR.js
@@ -0,0 +1,13 @@
+﻿export const GenrateQR = (item,id) => new QRious(
+        {
+             element: document.getElementById(id),
+             background: item.background,
+             backgroundAlpha: item.backgroundAlpha,
+             foreground: item.foreground,
+             foregroundAlpha: item.foregroundAlpha,
+             level: item.level,
+             size: item.size,
+             value: item.value,
+             padding: item.padding
+        }
+    );
